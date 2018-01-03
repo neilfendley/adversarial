2d52f05f514543c9a4c06cb3c81c2a5ce586c2f2
mjpekala <mpekala@umd.edu>
2018-01-03 09:14:02 -0500

    starting to add random crops.  still under construction...

--- LisaCNN/train_lisacnn.py
+++ LisaCNN/train_lisacnn.py
@@ -3,7 +3,6 @@
 
 import os
 import random
-import keras
 import math
 import json
 from PIL import Image
@@ -16,6 +15,8 @@ from sklearn.metrics import confusion_matrix
 import tensorflow as tf
 from tensorflow.python.platform import app
 from tensorflow.python.platform import flags
+
+import keras
 
 from cleverhans.attacks import FastGradientMethod
 from cleverhans.utils_keras import cnn_model
@@ -33,6 +34,7 @@ flags.DEFINE_string(
 flags.DEFINE_integer('nb_epochs', 60, 'Number of epochs to train model')
 flags.DEFINE_integer('nb_classes', 48, 'Number of classes')
 flags.DEFINE_integer('batch_size', 128, 'Size of training batches')
+flags.DEFINE_integer('input_dim', 38, 'Size of input images')
 flags.DEFINE_float('learning_rate', 0.0001, 'Learning rate for training')
 flags.DEFINE_float('epsilon', 20, 'FGSM perturbation constraint')
 flags.DEFINE_bool('DO_CONF', False, 'Generate the confusion matrix on the test set')
@@ -85,7 +87,8 @@ def makedirs_if_needed(dirname):
         os.makedirs(dirname)
 
 
-def load_lisa_data(annotation_file='~/Data/LISA/allAnnotations.csv'):
+
+def load_lisa_data(annotation_file='~/Data/LISA/allAnnotations.csv',sz=32):
     """
     Loads LISA data set.
     
@@ -101,18 +104,31 @@ def load_lisa_data(annotation_file='~/Data/LISA/allAnnotations.csv'):
         xtest: numpy array of test data
         ytest: numpy array of test labels
     """
-    si = subimage.parse_LISA(annotation_file)
-    train_idx, test_idx = si.train_test_split(.17, max_per_class=500)
-    print(si.describe(train_idx))
-    print(si.describe(test_idx))
+    cache_fn = './data.npz'
 
-    x_train, y_train = si.get_subimages(train_idx, (32,32))
-    x_train = np.array(x_train) # list -> tensor
+    if not os.path.exists(cache_fn):
+        si = subimage.parse_LISA(annotation_file)
+        train_idx, test_idx = si.train_test_split(.17, max_per_class=500)
+        print(si.describe(train_idx))
+        print(si.describe(test_idx))
 
-    x_test, y_test = si.get_subimages(test_idx, (32,32))
-    x_test = np.array(x_test) # list -> tensor
+        x_train, y_train = si.get_subimages(train_idx, (sz,sz))
+        x_train = np.array(x_train) # list -> tensor
 
-    # UPDATE: make test size a multiple of batch size due to CH issues in model_eval
+        x_test, y_test = si.get_subimages(test_idx, (sz,sz))
+        x_test = np.array(x_test) # list -> tensor
+        np.savez(cache_fn, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
+    else:
+        f = np.load(cache_fn)
+        x_train = f['x_train']
+        y_train = f['y_train']
+        x_test = f['x_test']
+        y_test = f['y_test']
+
+    #
+    # UPDATE:
+    # Make test size a multiple of batch size due to CH issues in model_eval
+    #
     rem = np.mod(x_test.shape[0], FLAGS.batch_size)
     if rem != 0:
         print('WARNING: trucating eval size by %d' % rem)
@@ -180,6 +196,7 @@ def old_load_lisa_data():
     return np.asarray(X_train), np.asarray(Y_train), np.asarray(X_test), np.asarray(Y_test)
 
 
+
 def data_lisa(per_class_limit=500):
     """
     Funtion to read in the data prepared by the lisa dataset
@@ -194,9 +211,7 @@ def data_lisa(per_class_limit=500):
     """
     # NOTE: it would appear the LISA annotation extraction code introduces some 
     #       label noise.  Therefore, we do the extraction ourselves.
-    #
-    #X_train, Y_train, X_test, Y_test = old_load_lisa_data()
-    X_train, Y_train, X_test, Y_test = load_lisa_data()
+    X_train, Y_train, X_test, Y_test = load_lisa_data(sz=FLAGS.input_dim)
 
     X_train = X_train.astype('float32')
     X_test = X_test.astype('float32')
@@ -222,20 +237,20 @@ def data_lisa(per_class_limit=500):
 
 
 
-def make_lisa_cnn(sess):
+def make_lisa_cnn(sess, batch_size, dim=32):
     """ Note that the network produced by cnn_model() is fairly weak.
         For example, on CIFAR-10 it gets 60-something percent accuracy,
         which substantially below state-of-the-art.
     """
     num_classes=48
-    x = tf.placeholder(tf.float32, shape=(None, 32, 32, 3))
-    y = tf.placeholder(tf.float32, shape=(None, num_classes))
+    x = tf.placeholder(tf.float32, shape=(batch_size, dim, dim, 3))
+    y = tf.placeholder(tf.float32, shape=(batch_size, num_classes))
 
-    # This affine transformation may not be strictly necessary
+    # Puts data into [-.5,.5]
     x = (x / 255.) - 0.5
 
-    # TODO: fix naming convention explicitly? 
-    # otherwise, names depend upon when model was created...
+    # XXX: set layer naming convention explicitly? 
+    #      Otherwise, names depend upon when model was created...
     model = cnn_model(img_rows=32, img_cols=32, channels=3, nb_classes=num_classes)
 
     return model, x, y
@@ -283,29 +298,31 @@ def run_in_batches(sess, x_tf, y_tf, output_tf, x_in, y_in, batch_size):
 def train_lisa_cnn(sess, cnn_weight_file):
     """ Trains the LISA-CNN network.
 
-    TODO: some synthetic data augmentation!!
     """
-    X_train, Y_train, X_test, Y_test = data_lisa()   # load data
-    model, x, y = make_lisa_cnn(sess)
+    model, x, y = make_lisa_cnn(sess, FLAGS.batch_size, FLAGS.input_dim)
+
+    # construct an explicit predictions variable
+    x_crop = tf.random_crop(x, (FLAGS.batch_size, 32, 32, 3))
+    model_output = model(x_crop)
+
+    X_train, Y_train, X_test, Y_test = data_lisa()
 
     def evaluate():
-        # Evaluate the accuracy of the lisaCNN model on legitimate test
-        # examples
+        # Evaluate accuracy of the lisaCNN model on clean test examples.
         eval_params = {'batch_size': FLAGS.batch_size}
-        accuracy = model_eval(sess, x, y, model(x), X_test, Y_test, args=eval_params)
+        accuracy = model_eval(sess, x, y, model_output, X_test, Y_test, args=eval_params)
         print('Test accuracy on legitimate test examples: ' + str(accuracy))
-        preds = run_in_batches(sess, x, y, model(x), X_test, Y_test, FLAGS.batch_size)
+        preds = run_in_batches(sess, x, y, model_output, X_test, Y_test, FLAGS.batch_size)
         acc2 = 1. * np.sum(np.argmax(preds, axis=1) == np.argmax(Y_test, axis=1)) / Y_test.shape[0]
         print('second check: ' + str(acc2))
 
+    # Note: model_train() will add some new (Adam-related) variables to the graph
     train_params = {
         'nb_epochs': FLAGS.nb_epochs,
         'batch_size': FLAGS.batch_size,
         'learning_rate': FLAGS.learning_rate
     }
-
-    # Note: i believe this will add some new (Adam-related) variables to the graph
-    model_train(sess, x, y, model(x), X_train, Y_train, evaluate=evaluate, args=train_params)
+    model_train(sess, x, y, model_output, X_train, Y_train, evaluate=evaluate, args=train_params)
 
     saver = tf.train.Saver()
     save_path = saver.save(sess, cnn_weight_file)
