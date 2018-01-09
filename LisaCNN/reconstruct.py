ee1bb9b1c1767a7d7b8b711101b593553e8871b1
mjpekala <mpekala@umd.edu>
2018-01-09 10:46:14 -0500

    updated based on changes to file name format

--- LisaCNN/reconstruct.py
+++ LisaCNN/reconstruct.py
@@ -4,7 +4,7 @@
 
    Example usage:
 
-     python reconstruct.py `find ./output/Images/Iterative_FGM_0.15 -name \*png -print`
+     python reconstruct.py `find ./output/Images/FGM-ellinf-0.150 -name \*png -print`
 """
 
 import os, sys
@@ -41,9 +41,7 @@ if __name__ == "__main__":
 
         # deterime the image id in the *test set*
         pieces = tail.split('_')
-        if pieces[0].startswith('image'):
-            pieces[0] = pieces[0][5:]
-        test_image_idx = int(pieces[0])
+        test_image_idx = int(pieces[1])
 
         # determine original image id in the full set
         full_image_id = test_indices[test_image_idx]
@@ -56,3 +54,4 @@ if __name__ == "__main__":
         out_fn = out_fn + "_full_image" + ext
         img = Image.fromarray(full_img.astype('uint8'))
         img.save(out_fn)
+        print(out_fn)
